b536658106ff5f991be3709d032af81378bbfec1
Data och Ljud <johan@dataochljud.se>
2018-11-04 12:53:55 +0100

    added jQuery

--- searchC.php
+++ searchC.php
@@ -1,11 +1,14 @@
 <?php require("includes/header.php"); ?>
 <h1>Sök bland kurser</h1>
-<?php require("includes/menu_buttons.php"); ?>
-<a href="searchC.php" class="btn btn-primary" role="button">Sök bland kurser</a>
-<a href="regC.php" class="btn btn-primary disabled" role="button">Registrera kurs</a>
+<p />
+<?php require("includes/menu_buttons.php"); 
+        echo '<br><input type="text" id="myInput" placeholder="Sök efter kurser..."><button id="sbutton">Sök</button>';
+?>
+<p />
+<div id="result">
  <?php $result = $conn->query("SELECT * FROM Kurs");
     if ($result->num_rows > 0) {
-        echo '<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Sök efter kurser...">';
+
         echo '<table class="table table-bordered table-hover" id="myTable">';
         echo '<thead><tr><th>Kurskod</th><th>Kursnamn</th><th>Anmälningskod</th><th>Hastighet</th><th>Högskolepoäng</th><th>Datum</th></tr></thead><tbody>';
         // output data of each row fg
@@ -19,33 +22,17 @@
     }
 
     $conn->close(); ?>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+</div>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script>
-function myFunction() {
-  // Declare variables 
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td_f = tr[i].getElementsByTagName("td");
-    for (j = 0; j < td_f.length; j++) {
-        td = td_f[j];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-  }
-}
+$(document).ready(function(){
+    $("#sbutton").click(function(){
+        s1 = $("#myInput").val();
+        $("#result").load("s_C.php");  
+    });
+});
 </script>
 </body>  
 </html>
